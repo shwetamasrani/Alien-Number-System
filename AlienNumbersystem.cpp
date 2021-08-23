1d4705731902ff136b705be6541b450872ba911d
shwetamasrani <shwetamasrani19@gmail.com>
2021-08-23 22:02:25 +0530

    Added Comments for readability

--- AlienNumbersystem.cpp
+++ AlienNumbersystem.cpp
@@ -1,30 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Function to find the successor of given input number in New Alien Number system provided by user
 string succ_alien(string input, string NumberSys)
 {
-    vector<char> alienNumSys;
+    vector<char> alienNumSys;   // To map Alien alphabet with digits
     string NumberInput = "", nextNum = "";
+
+    // Creating a dictionary to map Numbers with Alien Alphabet
     for(int i=0; i<NumberSys.size(); i++)
     {
         alienNumSys.push_back(NumberSys[i]);
     }
 
+    // Converting Input alient system number to digits of Base = Number of characters in Alien Number system
     for(int i=0; i<input.size(); i++)
     {
         auto it = find(alienNumSys.begin(), alienNumSys.end(), input[i]);
         NumberInput += to_string(it - alienNumSys.begin());
     }
 
+    // Adding 1 to the above number
     int carry=0, curr =0;
     for(int i = NumberInput.size()-1; i>=0; i--)
     {
         curr = (int)NumberInput[i] - 48 + carry; 
-        if(i == NumberInput.size() -1)
+        if(i == NumberInput.size() -1)          // Add one to last digit
         {
             curr += 1;
         }
-        if(curr == alienNumSys.size())
+        if(curr == alienNumSys.size())          // Checking for carry
         {
             curr = 0;
             carry = 1;
@@ -33,9 +38,9 @@ string succ_alien(string input, string NumberSys)
         {
             carry = 0;
         }
-        nextNum += alienNumSys[curr];
+        nextNum += alienNumSys[curr];           // Appeding the digits from right 
     }
-    reverse(nextNum.begin(), nextNum.end());
+    reverse(nextNum.begin(), nextNum.end());    // Reversing to get the origin Number
     return nextNum;
 }
 
@@ -44,10 +49,10 @@ int main()
 {
     string str, alienNum;
     cout<<"Enter Number System:\n";
-    cin>>alienNum;
+    cin>>alienNum;                  // Input Alien Number System
     cout<<"Enter a number of above number System:\n";
-    cin>>str;
+    cin>>str;                       // Input Number 
     cout<<"Next Number According to Above Alien Number system:\n";
-    cout<<succ_alien(str, alienNum);
+    cout<<succ_alien(str, alienNum);    //  Successor of given number in Alien Number System
     return 0;
 }
